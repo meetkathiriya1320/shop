38b96c56123819faed14d1d67f5bfb785d94f7ae
meetkathiriya1320 <meet.kathiriya1320@gmail.com>
2025-12-10 15:41:02 +0530

    change method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './src/routes/auth.js';
 import categoryRoutes from './src/routes/category.js';
@@ -16,6 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Serve static files from public folder
